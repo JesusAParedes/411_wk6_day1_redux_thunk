7b7704d637ac9bc8e8bdc4c4d1369059542e0a4c
JesusAParedes <paredesjesus93@gmail.com>
2023-04-13 19:17:27 -0500

    follow steps to add import utility to page

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -12,4 +12,39 @@ export const removeCar = (index) => {
         type: 'REMOVE_CAR',
         value: index
     }
-}
+};
+
+export const fetchMakes = () => {
+    return (dispatch) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(response => {
+          const action = {
+            type: 'FETCH_MAKES',
+            value: response.Results
+          }
+          dispatch(action)
+        })
+    }};
+
+export const deleteMakes = (index) => {
+    return {
+        type: 'REMOVE_MAKES',
+        value: index
+    }
+};
+
+
+
+// export const fetchMakes = () => {
+//     return async cars => {
+//         const response = await fetch(url);
+//         const action = (
+//             {
+//                 type: "FETCH_MAKES",
+//                 value: response.Results
+//             }
+//         )
+    
+//     };
+// };
